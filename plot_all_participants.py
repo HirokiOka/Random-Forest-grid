9fa43ae536dc8b244f51f62e298bf4bf1a637258
hiroki <hiro_bass.0827@icloud.com>
2022-06-16 13:36:00 +0900

    fix plotter

--- plot_all_participants.py
+++ plot_all_participants.py
@@ -17,7 +17,20 @@ print("train positive: ", len(y_train[y_train == 1]), "train negative: ", len(y_
 smote = SMOTE(sampling_strategy='auto', k_neighbors=5, random_state=42)
 X_train, y_train = smote.fit_resample(X_train, y_train)
 
+# code-related modal model
+"""
+model = RandomForestClassifier(
+        criterion='gini',
+        max_depth=490,
+        max_features=40,
+        n_estimators=40,
+        min_samples_leaf=1,
+        min_samples_split=70,
+        random_state=42
+        )
 
+"""
+# multi-modal model
 model = RandomForestClassifier(
         criterion='entropy',
         max_depth=180,
@@ -30,9 +43,15 @@ model = RandomForestClassifier(
 model.fit(X_train, y_train)
 
 fig, axes = plt.subplots(5, sharex="all", constrained_layout=True)
+plt.rcParams['font.family'] = 'Times New Roman'
+plt.rcParams["font.size"] = 12
+plt.rcParams["scatter.marker"] = ","
+plt.rcParams["scatter.edgecolors"] = None
+# plt.rcParams['figure.dpi'] = 300
 
 target_dir = './data/paritcipants/anon-task2'
 files = os.listdir(target_dir)
+files.sort()
 i = 0
 for filename in files:
     if (filename == '.DS_Store'):
@@ -44,16 +63,16 @@ for filename in files:
     y_test_pred = model.predict(X_test)
 
     plot_title = filename.split('.')[0]
-    axes[i].set_title(plot_title, fontsize=12)
+    axes[i].set_title(plot_title, fontsize=10)
     axes[i].set_ylim([-0.5, 1.5])
-    axes[i].scatter(x=range(len(y_test)), y=y_test+0.1, s=0.1, c='r', label='Ground-truth')
-    axes[i].scatter(x=range(len(y_test_pred)), y=y_test_pred, s=0.1, c='b', label='Prediction')
+    axes[i].scatter(x=range(len(y_test)), y=y_test+0.15, s=1, c='r', label='Ground-truth')
+    axes[i].scatter(x=range(len(y_test_pred)), y=y_test_pred, s=1, c='b', label='Prediction')
     i += 1
 
 lines, labels = fig.axes[-1].get_legend_handles_labels()
-fig.legend(lines, labels, loc='center right', markerscale=10)
+fig.legend(lines, labels, loc='upper right', markerscale=5, ncol=2, bbox_to_anchor=(1, 1), borderaxespad=0, fontsize=10)
 fig.supxlabel('elapsed seconds[s]')
 fig.supylabel('label')
 # plt.show()
 fname = 'multi_all_p.pdf'
-plt.savefig(fname, dpi='figure', format='pdf', metadata='pdf')
+plt.savefig(fname, format='pdf')
